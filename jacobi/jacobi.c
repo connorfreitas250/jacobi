0e4a58abae85aed8b955efd6781bbf1f0da770a1
freitac2 <freitac2@students.wwu.edu>
2016-05-28 18:29:57 -0700

    Done testing.

--- jacobi/jacobi.c
+++ jacobi/jacobi.c
@@ -80,11 +80,11 @@ int main(int argc, char* argv[]) {
     pthread_join(thds[i], &retval);
   }
   
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < N; ++j) {
-      printf("%.10lf ", grid[i][j]);
-    }
-  }
+  //for (int i = 0; i < N; ++i) {
+  //  for (int j = 0; j < N; ++j) {
+  //    printf("%.10lf ", grid[i][j]);
+  //  }
+  //}
   
   free(grid);
   free(newgrid); 
